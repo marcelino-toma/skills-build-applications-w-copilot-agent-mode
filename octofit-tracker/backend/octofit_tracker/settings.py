2b6e2ab6918ea984de66035a52b319e3322fe4a5
marcelino-toma <marcelinotoma.exe@gmail.com>
2026-05-05 13:36:26 +0000

    Update settings.py to support Codespace and localhost in ALLOWED_HOSTS

--- octofit-tracker/backend/octofit_tracker/settings.py
+++ octofit-tracker/backend/octofit_tracker/settings.py
@@ -10,7 +10,9 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -26,7 +28,13 @@ SECRET_KEY = 'django-insecure-x$l7dys@vg^6h511(jz$kr$*a_#g9%s47gf6^p_ze)sj)_u=2e
 DEBUG = True
 
 
-ALLOWED_HOSTS = ['*']
+
+# Support Codespace and localhost
+CODESPACE_NAME = os.environ.get('CODESPACE_NAME')
+codespace_host = f"{CODESPACE_NAME}-8000.app.github.dev" if CODESPACE_NAME else None
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+if codespace_host:
+    ALLOWED_HOSTS.append(codespace_host)
 
 
 # Application definition
